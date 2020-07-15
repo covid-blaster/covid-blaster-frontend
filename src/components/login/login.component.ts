3f1e021b40f9c4816486d7685d2afe47f89ebb54
Varun Chennamadhava <varunchennamadhava@varuns-mbp.home>
2020-07-15 13:58:41 -0400

    register form form not getting data. will have to check on it

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   extraVariable:boolean;
   truthVariable:boolean;
   loginForm: FormGroup;
+  registerForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
   ) { }
@@ -26,9 +27,24 @@ export class LoginComponent implements OnInit {
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
+    this.registerForm = this.formBuilder.group({
+      firstNameOne: ['', Validators.required],
+      lastNameOne: ['', Validators.required],
+      emailOne: ['', Validators.required],
+      passwordOne: ['', Validators.required],
+    });
+  }
+
+  newRegistrationClick() {
+    console.log(this.registerForm.value.firstName);
+    console.log(this.registerForm.value.lastName);
+    console.log(this.registerForm.value.email);
+    console.log(this.registerForm.value.password);
   }
 
   loginSubmitForm() {
+
+
     console.log(this.loginForm.value.email);
     console.log(this.loginForm.value.password);
   }
@@ -40,7 +56,6 @@ export class LoginComponent implements OnInit {
   changeExtraVariable() {
     this.truthVariable = true;
     this.extraVariable = false;
-
   }
 
   goBack() {
